0571f4c0b2d4691077ccd5db1220f1e3e3b7a628
DarraghMcKernan <dmckernan77@gmail.com>
2024-12-07 15:58:11 +0000

    laptop is dying 😢

--- Lab 6/Lab 6/AIDecisionMaker.cpp
+++ Lab 6/Lab 6/AIDecisionMaker.cpp
@@ -17,13 +17,16 @@ int AIDescisionMaker::pickCellToPlace()
 	return chosenCell;
 }
 
-int AIDescisionMaker::pickPieceToPlace() 
-{
-	//the more shapes that make up the piece the higher its value
+//the more shapes that make up the piece the higher its value
 	//collision check just needs to compare the matrix of the shape to the board to find if there is a slot for that particular piece
 	// 0,1,0      if the board has a series of cells that lines up with this we use that as one of its possible places
 	// 1,1,1      this also avoids sfmls collision check which is probably more expensive
-	// 0,1,0
+	// 0,1,0	  we will want to place the largest piece we can typically, if there is no space work down in size
+
+	// also can check if a position is optimal as it will be near a friendly tile - type 1 is invalid(wall), 2 is player, 3 is AI
+
+int AIDescisionMaker::pickPieceToPlace() 
+{
 	int bestPiece = currentPiece;
 	AIPiecesUsed[currentPiece]++;
 	if (currentPiece <= 3)
